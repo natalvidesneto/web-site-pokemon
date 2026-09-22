16162201b99c12ea2de5fa74eb40f2e40ef08ea0
Natalvides Neto <netonatalvides@gmail.com>
2026-09-22 17:50:51 -0300

    Start of the creation of the Pokédex page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
+import { Pokedex } from "./pages/Pokedex/Pokedex";
 
 function App(){
     return(
@@ -7,6 +8,7 @@ function App(){
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/Pokedex" element={<Pokedex/>}/>
         </Routes>
         </BrowserRouter>
         </>
---
+++ src/pages/Pokedex/Pokedex.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/Header/Header";
+
+export function Pokedex() {
+    return (
+        <>
+            <Header/>
+            <main>
+                <h1>Hello world!</h1>
+            </main>
+        </>
+    )
+}
